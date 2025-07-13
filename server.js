9f1991a5d8706b71d3b42aae1a63903fe69f75e5
sabri.bargougui <sabri.bargougui@intalio.com>
2025-07-13 15:18:28 +0100

    backend

--- server.js
+++ server.js
@@ -8,13 +8,18 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://sabribargougui.github.io',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use('/api', authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB Connected');
-    app.listen(3000, () => console.log('Server running on http://localhost:3000'));
-  })
-  .catch(err => console.error(err));
+    .then(() => {
+        console.log('MongoDB Connected');
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+    })
+    .catch(err => console.error(err));
